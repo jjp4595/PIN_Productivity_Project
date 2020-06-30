03358424956c0c03111a1ec0620c5b6cd7f57075
Jordan Pannell <jpannell1@sheffield.ac.uk>
2020-06-30 12:39:17 +0100

    ..

--- Scripts/synthetic_network.py
+++ Scripts/synthetic_network.py
@@ -21,6 +21,7 @@ import attractivity_modelling
 import fractal_working
 
 
+#Pickling functions
 def save_obj(obj, name ):
     with open('obj/'+ name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
@@ -32,6 +33,9 @@ def load_obj(name ):
 
 #--------------Loading data ---------------------------------------------------
 def sheff_import():
+    """
+    Function to generate the lsoa_data dictionary used throughout. 
+    """
     sheff_lsoa = {}
     sheff_lsoa['sheff_lsoa_shape'] = gpd.read_file(os.path.join(os.environ['USERPROFILE'] + r"\Dropbox\PIN\1 Data\1 Data\CDRC\Census Data Pack\Sheffield\shapefiles\Sheffield_lsoa11.shp"))
     sheff_lsoa['sheff_lsoa_pop'] = gpd.read_file(os.path.join(os.environ['USERPROFILE'] + r"\Dropbox\PIN\1 Data\1 Data\CDRC\Census Data Pack\Sheffield\tables\KS101EW_lsoa11.csv"))
@@ -43,205 +47,9 @@ def sheff_import():
     sheff_lsoa['sheff_lsoa_income'] = sheff_lsoa['sheff_lsoa_income'].iloc[ids]
     sheff_lsoa['sheff_lsoa_education'] = gpd.read_file(os.path.join(os.environ['USERPROFILE'] + r"\Dropbox\PIN\1 Data\1 Data\CDRC\Census Data Pack\Sheffield\tables\KS501EW_lsoa11.csv"))
     return sheff_lsoa
-#sheff_lsoa = sheff_import()
 
-
-#Graphing
-params = {'font.family':'serif',
-        'axes.labelsize':'small',
-        'xtick.labelsize':'x-small',
-        'ytick.labelsize':'x-small', 
-        'axes.linewidth':0.5,
-        
-        'xtick.major.width':0.5,
-        'xtick.minor.width':0.4,
-        'ytick.major.width':0.5,
-        'ytick.minor.width':0.4,
-        'xtick.major.size':3.0,
-        'xtick.minor.size':1.5,
-        'ytick.major.size':3.0,
-        'ytick.minor.size':1.5,
-        
-        'legend.fontsize':'small',
-        'legend.title_fontsize':'small',
-        'legend.fancybox': False,
-        'legend.framealpha': 1,
-        'legend.shadow': False,
-        'legend.frameon': True,
-        'legend.edgecolor':'black',
-        'patch.linewidth':0.5,
-        
-        'scatter.marker': 's',
-        
-        'grid.linewidth':'0.5',
-        
-        'lines.linewidth':'0.5'}
-plt.rcParams.update(params)
-
-
-def income_edu_dists():
-    
-    #Graphing data
-    lsoa_data = load_obj("lsoa_data")
-    #Sense check distributions
-    
-    # fig, axs = plt.subplots(15,23)
-    # axs = axs.ravel()
-    # for i in range(len(lsoa_data['income_params'])):
-    #     i
-    #     r = stats.beta.rvs(lsoa_data['income_params'][i,0], lsoa_data['income_params'][i,1], loc =lsoa_data['income_params'][i,2], scale = lsoa_data['income_params'][i,3], size = 1000)
-    #     axs[i].plot(np.linspace(0,1,100),  stats.beta.pdf(np.linspace(0,1,100), lsoa_data['income_params'][i, 0], lsoa_data['income_params'][i, 1], loc = lsoa_data['income_params'][i, 2], scale = lsoa_data['income_params'][i, 3]) , label = 'CDF')
-    #     axs[i].hist(r, density = True)
-    # plt.tight_layout()
-    
-    # fig, axs = plt.subplots(15,23)
-    # axs = axs.ravel()
-    # for i in range(len(lsoa_data['income_params'])):
-    #     education=np.random.choice(4, size = lsoa_data['edu_counts'][i], p=lsoa_data['edu_ratios'][i]) #where p values are effectively the ratio of people with a given education level you can alternatively use the same method for income as well    
-    #     axs[i].hist(education, density = True)
-    # plt.tight_layout()
-    sns.set_palette("deep")
-    sns.set_color_codes()
-    maxid, minid = 103, 337 #income means max/min lsoa id
-
-
-    fig=plt.figure()
-    fig.set_size_inches(2,3)
-    gs = gridspec.GridSpec(2, 1, height_ratios=[10,1], width_ratios=[1], wspace=0.25, hspace=0)
-    ax0 = fig.add_subplot(gs[0])
-    ax1 = fig.add_subplot(gs[1], sharex = ax0)
-    inc1 = stats.beta.rvs(lsoa_data['income_params'][minid,0], lsoa_data['income_params'][minid,1], loc =lsoa_data['income_params'][minid,2], scale = lsoa_data['income_params'][minid,3], size = 1000)
-    sns.distplot(inc1, color='b', kde=False, norm_hist=True, ax=ax0)
-    ax0.plot(np.linspace(0,1,100),  stats.beta.pdf(np.linspace(0,1,100), lsoa_data['income_params'][minid, 0], lsoa_data['income_params'][minid, 1], loc = lsoa_data['income_params'][minid, 2], scale = lsoa_data['income_params'][minid, 3]) , label = 'CDF')
-    ax0.axvline(x=np.median(inc1), ls='--', lw=2)
-    sns.pointplot(data = inc1, ci = 'sd', orient = 'h', scale=2, ax=ax1)
-    ax0.set_xlim(0,1)
-    ax0.tick_params(axis='both', which='both',bottom=False, labelbottom=False)   
-    ax1.tick_params(axis='both', which='both',left=False,labelleft = False)   
-    ax1.set_xlabel('Normalised income')
-    plt.tight_layout()
-    
-    fig=plt.figure()
-    fig.set_size_inches(2,3)
-    gs = gridspec.GridSpec(2, 1, height_ratios=[10,1], width_ratios=[1], wspace=0.25, hspace=0)
-    ax0 = fig.add_subplot(gs[0])
-    ax1 = fig.add_subplot(gs[1], sharex = ax0)
-    inc2 = stats.beta.rvs(lsoa_data['income_params'][maxid,0], lsoa_data['income_params'][maxid,1], loc =lsoa_data['income_params'][maxid,2], scale = lsoa_data['income_params'][maxid,3], size = 1000)
-    sns.distplot(inc2, color='b', kde=False, norm_hist=True, ax=ax0)
-    ax0.plot(np.linspace(0,1,100),  stats.beta.pdf(np.linspace(0,1,100), lsoa_data['income_params'][maxid, 0], lsoa_data['income_params'][maxid, 1], loc = lsoa_data['income_params'][maxid, 2], scale = lsoa_data['income_params'][maxid, 3]) , label = 'CDF')
-    ax0.axvline(x=np.median(inc2), ls='--', lw=2)
-    sns.pointplot(data = inc2, ci = 'sd', orient = 'h', scale=2, ax=ax1)
-    ax0.set_xlim(0,1)
-    ax0.tick_params(axis='both', which='both',bottom=False, labelbottom=False)   
-    ax1.tick_params(axis='both', which='both',left=False,labelleft = False)   
-    ax1.set_xlabel('Normalised income')
-    plt.tight_layout()
-   
-
-
-    
-    
-    fig, [ax0, ax1] = plt.subplots(1,2)
-    fig.set_size_inches(4,2.5)
-    edu1=np.random.choice(4, size = 1000, p=lsoa_data['edu_ratios'][minid]) #where p values are effectively the ratio of people with a given education level you can alternatively use the same method for income as well    
-    sns.countplot(edu1, color = 'b', ax=ax0)
-    ax0.set_ylabel('Count')
-    ax0.set_xlabel('Highest education level')
-    plt.tight_layout()
-    
-    edu2=np.random.choice(4, size = 1000, p=lsoa_data['edu_ratios'][maxid])
-    sns.countplot(edu2, color='b', ax=ax1)
-    ax1.set_ylabel('Count')
-    ax1.set_xlabel('Highest education level')
-    plt.tight_layout()
-    
-    attractivity1=inc1**(-edu1)   
-    attractivtiy1_powerlaw = powerlaw.Fit(attractivity1)
-    attractivtiy1_powerlaw_temp = powerlaw.Fit(attractivity1, xmin=1)
-    fig = plt.figure()
-    fig.set_size_inches(2.5,2.5)
-    powerlaw_plot=attractivtiy1_powerlaw_temp.plot_ccdf(original_data=True, color='b', marker='.', ms=2, lw=0, alpha=0.5)
-    X=attractivtiy1_powerlaw_temp.ccdf()
-    x=X[0][X[0]>=attractivtiy1_powerlaw.xmin]
-    y=[np.exp( (-(attractivtiy1_powerlaw.alpha-1)*(np.log(i)-np.log(attractivtiy1_powerlaw.xmin))+np.log(X[1][X[0]==attractivtiy1_powerlaw.xmin])) ) for i in x]
-    plt.plot(x, y, 'k')
-    plt.ylabel(r'$P(attractivity \geq x)$')
-    plt.xlabel(r'$attractivity$')
-    plt.tight_layout()
-    
-    attractivity2=inc2**(-edu2)   
-    attractivtiy2_powerlaw = powerlaw.Fit(attractivity2)
-    attractivtiy2_powerlaw_temp = powerlaw.Fit(attractivity2, xmin=1)
-    fig = plt.figure()
-    fig.set_size_inches(2.5,2.5)
-    powerlaw_plot=attractivtiy2_powerlaw_temp.plot_ccdf(original_data=True, color='b', marker='.', ms=2, lw=0, alpha=0.5)
-    X=attractivtiy2_powerlaw_temp.ccdf()
-    x=X[0][X[0]>=attractivtiy2_powerlaw.xmin]
-    y=[np.exp( (-(attractivtiy2_powerlaw.alpha-1)*(np.log(i)-np.log(attractivtiy2_powerlaw.xmin))+np.log(X[1][X[0]==attractivtiy2_powerlaw.xmin])) ) for i in x]
-    plt.plot(x, y, 'k')
-    plt.ylabel(r'$P(attractivity \geq x)$')
-    plt.xlabel(r'$attractivity$')
-    plt.tight_layout()
-    
-    paths_matrix = load_obj("ave_paths")
-    paths = paths_matrix.flatten()
-    centroid_info = load_obj("centroids_beta_params_centroiddists")
-    centroid_paths = np.vstack(centroid_info['euclidean_dists']).flatten()
-    
-    fig=plt.figure()
-    fig.set_size_inches(3,3)
-    gs = gridspec.GridSpec(2, 1, height_ratios=[10,1], width_ratios=[1], wspace=0.25, hspace=0)
-    ax0 = fig.add_subplot(gs[0])
-    ax1 = fig.add_subplot(gs[1], sharex = ax0)
-    sns.distplot(centroid_paths, color='b', kde=False,  ax=ax0)
-    ax0.axvline(x=np.median(centroid_paths), ls='--', lw=2)
-    sns.pointplot(data = centroid_paths, ci = 'sd', orient = 'h', scale=2, ax=ax1)
-    ax0.set_xlim(0,30000)
-    ax0.tick_params(axis='both', which='both',bottom=False, labelbottom=False)   
-    ax1.tick_params(axis='both', which='both',left=False,labelleft = False)   
-    ax1.set_xlabel('Centroid path distances')
-    plt.tight_layout()
-    
-    fig=plt.figure()
-    fig.set_size_inches(3,3)
-    gs = gridspec.GridSpec(2, 1, height_ratios=[10,1], width_ratios=[1], wspace=0.25, hspace=0)
-    ax0 = fig.add_subplot(gs[0])
-    ax1 = fig.add_subplot(gs[1], sharex = ax0)
-    sns.distplot(paths, color='b', kde=False,  ax=ax0)
-    ax0.axvline(x=np.median(paths), ls='--', lw=2)
-    sns.pointplot(data = paths, ci = 'sd', orient = 'h', scale=2, ax=ax1)
-    ax0.set_xlim(0,30000)
-    ax0.tick_params(axis='both', which='both',bottom=False, labelbottom=False)   
-    ax1.tick_params(axis='both', which='both',left=False,labelleft = False)   
-    ax1.set_xlabel('Driveable path distance')
-    plt.tight_layout()
-    
-    fig=plt.figure()
-    fig.set_size_inches(3,3)
-    gs = gridspec.GridSpec(2, 1, height_ratios=[10,1], width_ratios=[1], wspace=0.25, hspace=0)
-    ax0 = fig.add_subplot(gs[0])
-    ax1 = fig.add_subplot(gs[1], sharex = ax0)
-    diffs = abs(paths-centroid_paths)
-    sns.distplot(diffs, color='b', kde=False,  ax=ax0)
-    ax0.axvline(x=np.median(diffs), ls='--', lw=2)
-    sns.pointplot(data = diffs, ci = 'sd', orient = 'h', scale=2, ax=ax1)
-    ax0.tick_params(axis='both', which='both',bottom=False, labelbottom=False)   
-    ax0.set_xlim(0,7000)
-    ax1.tick_params(axis='both', which='both',left=False,labelleft = False)   
-    ax1.set_xlabel('Difference in distances')
-    plt.tight_layout()
-    
-def plotgraph():
-    import osmnx as ox
-    import path_querying
-    G1 = path_querying.load_graph()
-    ox.plot.plot_graph(G1, dpi = 600, node_color = 'None', fig_width=6, fig_height=6)
-    
-#------------------------------------------------------------------------------
- 
-                                
-
-#Sampling attractivities --------------------------------------
+                               
+#Monte carlo functions -------------------------------------------------------
 def sample_attractivities(edu_ratios, income_params, fit = None):  
 
     attractivity1 = np.zeros((len(income_params)))
@@ -258,6 +66,7 @@ def sample_attractivities(edu_ratios, income_params, fit = None):
         return attractivity1, attractivity2, alpha, xmin
     else:
         return attractivity1, attractivity2
+
 
 def euclidean_dists_fun(sheff_shape): 
     """
@@ -281,8 +90,6 @@ def euclidean_dists_fun(sheff_shape):
     med_paths = int(med_paths[int(len(med_paths)/2)])
     
     return euclidean_dists, all_coords, paths_matrix, med_paths
-#euclidean_dists, centroids, paths_matrix, med_paths = euclidean_dists_fun()
-
 
 
 def fractal_dimension(coords_data):
@@ -322,7 +129,6 @@ def fractal_dimension(coords_data):
     return Df
 
 
-
 #Shuffling data 
 def paths_shuffle(shape, income_params):
     """
@@ -335,8 +141,33 @@ def paths_shuffle(shape, income_params):
 
 
 
-#Monte Carlo run throughs ----------------------------------------------------
+#Monte Carlo function---------------------------------------------------------
 def monte_carlo_runs(m, n, lsoa_data, paths_matrix, is_shuffled=None):
+    """
+
+    Parameters
+    ----------
+    m : TYPE
+        DESCRIPTION.
+    n : TYPE
+        DESCRIPTION.
+    lsoa_data : TYPE
+        DESCRIPTION.
+    paths_matrix : TYPE
+        DESCRIPTION.
+    is_shuffled : TYPE, optional
+        DESCRIPTION. The default is None.
+
+    Returns
+    -------
+    UrbanY : List of n values
+        Output values to be interpreted.
+    edge_freq : List of n numpy arrays 
+        How often an edge ocurrs between an lsoa pair.
+    edge_width : List of n numpy arrays
+        Ave. strength of an lsoa edge
+
+    """
     startt = time.time()
     time_log = []  
     
@@ -444,26 +275,28 @@ def monte_carlo_runs(m, n, lsoa_data, paths_matrix, is_shuffled=None):
 
 #------------------------------------------------------------------------------
 #
-#                   Running Monte Carlo
+#                   Multiprocessing - Running Monte Carlo
 #
 #------------------------------------------------------------------------------
 import multiprocessing
 if __name__ == '__main__':
+    
     #imports
     lsoa_data = load_obj("lsoa_data")
     import scipy.io as sio 
     mldata = sio.loadmat(r'C:\Users\cip18jjp\Dropbox\PIN\hadi_scripts\optimisedpaths.mat')#import new paths
-    n = 2000
-
-    # 2a)--------------------------------------
-    # Normal paths, m = [0.5, 1, 2]
+    n = 2000 #number of monte carlo repeats
+    ms = [0.5, 1, 1.5, 2] #m values for analysis
+    
+    # -----------------------------------------
+    # Normal paths
     # -----------------------------------------
     
     
     t1 = time.time()
     no_scripts = multiprocessing.cpu_count()
     
-    ms = [0.5, 1, 1.5, 2]
+    
     
     paths_matrix = load_obj("ave_paths")
     args_normal = []
@@ -492,8 +325,8 @@ if __name__ == '__main__':
     print(time.time()-t1)
 
 
-    #3a)--------------------------------------
-    # #minmax paths, m = [0.5, 1, 2]
+    #-----------------------------------------
+    # #minmax paths
     #-----------------------------------------
     
     paths = mldata['minmax']
@@ -534,8 +367,8 @@ if __name__ == '__main__':
     
     print(time.time()-t1)
 
-    #3b)--------------------------------------
-    # #slmin straightline paths, m = [0.5, 1, 2]
+    #-----------------------------------------
+    # #slmin straightline paths
     #-----------------------------------------
     paths = mldata['slmin']
     paths = np.reshape(paths, (len(paths)))
@@ -580,8 +413,8 @@ if __name__ == '__main__':
     print(time.time()-t1)
 
 
-    #3c)--------------------------------------
-    # #+- 50% paths, m = [0.5, 1, 2]
+    #-----------------------------------------
+    # #+- 50% paths
     #-----------------------------------------
     paths = mldata['od50']
     paths = np.reshape(paths, (len(paths)))
